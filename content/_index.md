7ec096df83ebe92a7045059f30e98bf7f971b0a5
Kris <madden_kris@yahoo.com>
2020-09-25 17:44:44 +0200

    before adding new content

--- content/_index.md
+++
@@ -1,9 +0,0 @@
----
-title: "What is the BabyLogger"
-weight: 100
-description: >
-  >
-  Use hugo shortcodes to quickly compose site pages.
----
-
-# index file
--- themes/coml-theme/assets/js/script.js
+++ themes/coml-theme/assets/js/script.js
@@ -1,18 +1,26 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    const observerForTableOfContentActiveState = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            const id = entry.target.getAttribute('id');
+    const options = {
+        root: null, // it is the viewport by default
+        threshold: 0, // 0-1 scale. 0 by default. if threshold is at 1, 100% of item has to be on the page
+        rootMargin: "30px 0px -75% 0px" // pulls root down (or up) needs to be in pixels or percentages (no just 0)
+    }
 
+    const observerForTableOfContentActiveState = new IntersectionObserver(function
+        (entries, observer) {
+            entries.forEach(entry => {
+                console.log(entry);
+                const id = entry.target.getAttribute('id');
             if (entry.intersectionRatio > 0) {
                 
                 clearActiveStatesInTableOfContents();
 
                 
                 document.querySelector(`ul li a[href="#${id}"]`).parentElement.classList.add('active');
+                hidesubmenus();
             }
-        });
-    });
+            });
+        }, options);
 
     
     document.querySelectorAll('h1[id],h2[id],h3[id],h4[id]').forEach((section) => {
@@ -28,34 +36,76 @@ function clearActiveStatesInTableOfContents() {
 
 
     // Smooth scrolling for browsers that don't support CSS smooth scrolling
-if (window.getComputedStyle(document.documentElement).scrollBehavior !== 'smooth') {
-    document.querySelectorAll('a[href^="#"]').forEach(internalLink => {
-        const targetElement = document.querySelector(internalLink.getAttribute('href'));
-        if (targetElement) {
-            internalLink.addEventListener('click', (e) => {
-                targetElement.scrollIntoView({
-                    behavior: 'smooth',
+    if (window.getComputedStyle(document.documentElement).scrollBehavior !== 'smooth') {
+        document.querySelectorAll('a[href^="#"]').forEach(internalLink => {
+            const targetElement = document.querySelector(internalLink.getAttribute('href'));
+            if (targetElement) {
+                internalLink.addEventListener('click', (e) => {
+                    targetElement.scrollIntoView({
+                        behavior: 'smooth',
+                    });
+                    e.preventDefault();
                 });
-                e.preventDefault();
-            });
+            }
+        });
+    }
+
+
+    if (!$("h4").hasClass("subheading")) {
+        $("h4").addClass('subheading');
+        $(".subheading").before("<div class='section-divider'></div>");
+        $('h4').wrapInner('<span class="padmarge" />')
+    }
+
+
+    if (!$("h1").hasClass("needs-divider")) {
+        $("h1").addClass('needs-divider');
+        $("h1").before("<div class='section-divider-dark'></div>");
+    }
+
+    if (!$("h3").hasClass("needs-divider")) {
+        $("h3").addClass('needs-divider');
+        $("h3").before("<div class='section-divider-dark'></div>");
+    }
+
+
+    if (!$("h2").hasClass("needs-divider")) {
+        $("h2").addClass('needs-divider');
+        $("h2").before("<div class='section-divider-dark'></div>");
+    }
+
+    $( ".active" ).parent( ".firstLevel" ).addClass("hereitis");
+
+    $("#TableOfContents > ul > li").addClass("firstLevel");
+    $(".firstLevel > ul > li").addClass("secondLevel");
+    $(".secondLevel > ul > li").addClass("thirdLevel");
+    $(".firstLevel > a").addClass("firstBold");
+}
+
+
+function hidesubmenus() {
+
+    // if(!$(this ).children().hasClass("active")){
+    //     $(this).removeClass("hereitis");
+    // };
+    
+    $(".firstLevel.active").addClass("hereitis");
+
+    $(".secondLevel.active").parents(".firstLevel").addClass("hereitis");
+    $(".thirdLevel.active").parents(".firstLevel").addClass("hereitis");
+
+    $( ".firstLevel" ).each(function() {
+        if ($(this).hasClass("hereitis")) {
+            $(this).children().css({"display": "initial"});
+        }else{
+            $(this).children().not('a').css({"display": "none"});
         }
+        $(".firstLevel").children(".active").addClass("hereitis");
+        if(!$(".firstLevel.hereitis").hasClass("active") ){
+
+            $(this).removeClass("hereitis");
+        };
+
     });
-}
+};
 
-// $( "<div class='section-divider'></div>" ).insertBefore( "h4" );
-// var subheading = document.getElementsByTagName('h4')[3].innerHTML
-// $( "<div class='section-divider'></div>" ).insertBefore( subheading );
-// $('<div />').addClass('section-divider').insertBefore('h4');
-
-if (!$("h4").hasClass("subheading")) {
-    $("h4").addClass('subheading');
-    $(".subheading").prepend("<div class='section-divider'></div>");
-}
-
-if (!$(".section").hasClass("needs-divider")) {
-    $(".section").addClass('needs-divider');
-    $(".section").prepend("<div class='section-divider-dark'></div>");
-}
-
-
-}
